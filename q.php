08d2a23f9fedeaa3460eaf97eb514d3b46608b37
tapanmeena <tapanmeena1998@gmail.com>
2017-10-28 01:12:48 +0530

    Add files via upload

---
+++ q.php
@@ -0,0 +1,14 @@
+<?php
+//Step1
+ $db = mysqli_connect('localhost','username','password','database_name')
+ or die('Error connecting to MySQL server.');
+?>
+
+<html>
+ <head>
+ </head>
+ <body>
+ <h1>PHP connect to MySQL</h1>
+</body>
+</html>
+
